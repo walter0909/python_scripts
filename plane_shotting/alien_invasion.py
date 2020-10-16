a61013d992d973d4a9248f986272b46e601e0120
root <root@localhost.localdomain>
2020-10-16 10:03:24 +0800

    射击小游戏

---
+++ plane_shotting/alien_invasion.py
@@ -0,0 +1,28 @@
+
+
+import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+from pygame.sprite import Group
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Walter games")
+    
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets) 
+
+
+run_game()
---
+++ plane_shotting/ship.py
@@ -0,0 +1,45 @@
+import pygame
+
+class Ship():
+    
+    def __init__(self, ai_settings, screen):
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('images/gg.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        
+        self.centerxx = float(self.rect.centerx)
+        self.centeryy = float(self.rect.centery)
+
+
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_top = False
+        self.moving_bottom = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.centerxx += self.ai_settings.ship_speed_factor
+#            self.rect.centerx += 1
+        if self.moving_left and self.rect.left > 0:
+            self.centerxx -= self.ai_settings.ship_speed_factor
+#            self.rect.centerx -= 1
+        if self.moving_top and self.rect.top > 0:
+            self.centeryy -= self.ai_settings.ship_speed_factor
+#            self.rect.centery = 1
+        if self.moving_bottom and self.rect.bottom < self.screen_rect.bottom:
+            self.centeryy += self.ai_settings.ship_speed_factor
+#            self.rect.centery -= 1
+
+        self.rect.centerx = self.centerxx
+        self.rect.centery = self.centeryy
+
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
